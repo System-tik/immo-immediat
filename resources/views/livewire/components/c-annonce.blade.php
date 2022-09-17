d55c8ef94406626ffbc80101dcfd032fe70ddb81
king <glodin.kiese@gmail.com>
2022-09-17 16:27:23 +0100

    vers hiram

--- resources/views/livewire/components/c-annonce.blade.php
+++ resources/views/livewire/components/c-annonce.blade.php
@@ -1,6 +1,6 @@
 <a class="flex flex-col transition duration-200 transform shadow rounded-3xl group" href="#">
     {{-- Stop trying to control. --}}
-    <div class="flex flex-col justify-between px-5 transition duration-500 transform bg-cover rounded-3xl h-52 group-hover:h-60" style=" background-image:url('{{asset('img/maison/191199962_327186845676843_5614446053251912539_n.jpg')}}')">
+    <div class="relative flex flex-col justify-between px-5 transition duration-500 transform bg-cover rounded-3xl h-52 group-hover:h-60" style=" background-image:url('{{asset('img/maison/191199962_327186845676843_5614446053251912539_n.jpg')}}')">
         <div class="flex gap-2 py-5">
             <div class="px-2 text-white rounded-lg bg-t-black">10 Photo(s)</div>
             <div class="px-2 text-white rounded-lg bg-t-black">Favoris </div>
