97dfb68bbc0a2fdddb0a4216503c8ba2fcbb5fab
kingfac <glodin.kiese@gmail.com>
2023-02-22 10:16:50 +0100

    glodi detail

--- resources/views/livewire/client/v-liste-produit.blade.php
+++ resources/views/livewire/client/v-liste-produit.blade.php
@@ -2,6 +2,7 @@
 <div class="flex flex-col">
     <div class="grid gap-10 xl:grid-cols-4 md:grid-cols-2 lg:grid-cols-3">
         {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+        
         @foreach ($biens as $b)
         <livewire:components.c-annonce :bien="$b">
         @endforeach
